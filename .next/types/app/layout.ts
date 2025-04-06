659de10b267790436b70daaf607cdea59f299d57
30210200 <vraj.dudhat@ucalgary.ca>
2025-04-06 00:47:14 -0600

    Made some necessary installation to run the project on my local device; rectified internal/external errors disrupting project from running.

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/abdullahanwar/Desktop/UrbanSystems/app/layout.tsx
+// File: C:\Users\vrajd\IdeaProjects\timeline_project\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/abdullahanwar/Desktop/UrbanSystems/app/page.tsx
+// File: C:\Users\vrajd\IdeaProjects\timeline_project\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
